b5e5a6149c0ff2bfe623439b0b8f5b36d864ffbe
saikrishnaK <saikrishna.kilari@infor.com>
2016-02-11 18:19:07 +0530

    add

--- src/pack1/class1.java
+++ src/pack1/class1.java
@@ -1,8 +1,10 @@
+package pack1;
 
-public class TestClass {
+public class class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("ABCDEFGH");
 
 	}
 
